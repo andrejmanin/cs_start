8c39c33eaea2b59111457b299f9522000cb49061
andrewmanin <andrejmanin54@gmail.com>
2024-11-12 20:16:25 +0200

    Pass task 1

--- Program.cs
+++ Program.cs
@@ -6,51 +6,9 @@ namespace cs_start
     {
         static void Main(string[] args)
         {
-            /*try
-            {
-                int padding = Console.BufferWidth / 2;
-                int lineLength = Console.BufferHeight / 2;
-                
-                Console.SetWindowSize(Console.BufferWidth, Console.BufferHeight);
-                Console.SetCursorPosition(0, Console.CursorTop);
-                Write("Enter name: ");
-                Console.SetCursorPosition(12, Console.CursorTop);
-                string? name = ReadLine();
-                Console.Clear();
-                Console.SetCursorPosition(0, Console.CursorTop);
-                Write("Enter age: ");
-                Console.SetCursorPosition(12, Console.CursorTop);
-                int? age = Convert.ToInt32(ReadLine());
-                Console.Clear();
-                Console.SetCursorPosition(0, Console.CursorTop);
-                WriteLine($"Hello, {name}");
-                Console.SetCursorPosition(0, Console.CursorTop);
-                WriteLine($"Your age is: {age}");
-
-            }
-            catch (Exception ex)
-            {
-                WriteLine(ex.Message);
-            }*/
-            while (true)
-            {
-                
-            }
-
-            do
-            {
-                
-            } while (true);
-
-            var list = new List<String>()
-            {
-                "Hello",
-                "World",
-            };
-            foreach (var VARIABLE in list)
-            {
-                Console.WriteLine(VARIABLE);
-            }
+            string text =
+                "It's easy to win forgiveness for being wrong;\nbeing right is what gets you into real trouble.\nBjarne Stroustrup";
+            WriteLine(text);
         }
     }
 }
