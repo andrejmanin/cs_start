8e75268890fe5a2647cb050ed4d68c38523fcfee
andrewmanin <andrejmanin54@gmail.com>
2024-11-13 23:15:24 +0200

    Ended task

--- Program.cs
+++ Program.cs
@@ -6,31 +6,73 @@ namespace cs_start
     {
         static void Main(string[] args)
         {
-            try 
+            try
             {
-                Int32? start = Convert.ToInt16(Console.ReadLine());
-                Int32? end = Convert.ToInt16(Console.ReadLine());
-
-                if (start == 0 && end == 0)
+                // First variant
+                /*WriteLine("Please enter a line (to stop enter after symbol \\n): ");
+                int index = 0;
+                string? input;
+                do
                 {
-                    WriteLine($"{end} is a zero");
-                    return;
-                }
-
-                if (start > end)
-                {
-                    (start, end) = (end, start);
-                }
-
-                WriteLine();
-                for (int i = Convert.ToInt32(start); i <= end; i++)
-                {
-                    for (int j = 0; j < i; j++)
+                    index++;
+                    input = ReadLine();
+                    if (input.Length > 3)
                     {
-                        Write($"{i} ");
+                        if (input[input.Length - 2] == '\\' && input[input.Length - 1] == 'n')
+                        {
+                            break;
+                        }   
                     }
-                    WriteLine();
+
+                    if (input[0] == '\\' && input[1] == 'n')
+                    {
+                        break;
+                    }
+                    int length = input.Length;
+                    if (index > 1 && input.Length == 3){
+                        if(input[1] == '\\' && input[2] == 'n')
+                        {
+                            break;
+                        }
+                        WriteLine("Invalid input.");
+                        return;
+                    }
+                } while (true);
+
+                if (index > 1)
+                {
+                    WriteLine("Your line is vertical");
+                    WriteLine($"The symbol you entered was: {input[0]}");
+                    WriteLine($"The length your line is: {index}");
                 }
+                else
+                {
+                    WriteLine("Your line is horizontal");
+                    WriteLine($"The symbol you entered was: {input[0]}");
+                    WriteLine($"The length your line is: {input.Length - 2}"); // Last two elements are "\n"
+                }*/
+                
+                // Second variant
+                /*Write("Enter a if you want vertical line 'v' or 'h' for horizontal: ");
+                char lineChoise = Convert.ToChar(Console.ReadLine());
+                Write("Enter length your line: ");
+                int lineLength = Convert.ToInt32(Console.ReadLine());
+                Write("Enter width your symbol: ");
+                char symbol = Convert.ToChar(Console.ReadLine());
+
+                if (lineChoise == 'v')
+                {
+                    for (int i = 0; i < lineLength; i++)
+                    {
+                        WriteLine(symbol);
+                    }
+                } else if (lineChoise == 'h')
+                {
+                    for (int i = 0; i < lineLength; i++)
+                    {
+                        Write(symbol);
+                    }
+                }*/
             }
             catch (Exception e)
             {
